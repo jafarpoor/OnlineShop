597958bc2f92f5ed0a3f4bc9b25b82ff5524b0df
unknown <faezehjafarpour75@gmail.com>
2023-07-09 22:20:04 +0330

    dbContext

---
+++ OnlineShopProject/Application/Interface/IDataBaseContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Interface
+{
+    public interface IDataBaseContext
+    {
+
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ OnlineShopProject/Persistence/Context/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context
+{
+    public class DataBaseContext : DbContext , IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
